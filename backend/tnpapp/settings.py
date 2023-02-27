8b19a14858f9e3983820e38bd1531d94f370f339
jeelpatel231 <jeelpatel231@gmail.com>
2023-02-28 00:15:29 +0530

    add django email smtp details for sending email

--- backend/tnpapp/settings.py
+++ backend/tnpapp/settings.py
@@ -151,3 +151,11 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # use the custom user model
 AUTH_USER_MODEL = 'user.User'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
