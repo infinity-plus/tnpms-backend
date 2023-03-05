61ad1f6be1e9c20d0f0fd88d15770f4948bf74c6
jeelpatel231 <jeelpatel231@gmail.com>
2023-03-06 00:19:34 +0530

    simplify model structures

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,43 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from typing import Optional, Iterable
-
-# from user import managers
-from user.roles import Role, VolunteerType
+from django.contrib.auth.models import AbstractUser, UserManager
+from user.roles import VolunteerType
 from django.contrib.auth.models import Permission
 from functools import partial
 from typing import List
 from user.validators import number_validator
 from utils.models import Approvable
+
 # Create your models here.
 
 
 class User(AbstractUser, Approvable):
-    """
-    `User` table contains ALL the users in the system.
-    other classes like `Admin` `Student` `Volunteer` inherit from this class
-    and are saved as a pointer to original `User` in table with other properties
-    saved in their own table
-
-    Hence, the _base_role is Undefined, Roles can be found at `users.roles.Role`
-    """
-
-    _base_role: Role = Role.UNDEFINED
     _predefined_permissions: List[str] = []
 
-    role = models.PositiveSmallIntegerField(choices=Role.choices)
-    phone_number = models.CharField(max_length=10, validators=[
-                                    partial(number_validator, length=10)])
+    phone_number = models.CharField(
+        max_length=10, validators=[partial(number_validator, length=10)]
+    )
 
-    def save(
-        self,
-        force_insert: bool = False,
-        force_update: bool = False,
-        using: Optional[str] = None,
-        update_fields: Optional[Iterable[str]] = None,
-    ) -> None:
-        self.role = self._base_role
-        saved_user = super().save(force_insert, force_update, using, update_fields)
+    def save(self, *args, **kwargs) -> None:
+        saved_user = super().save(*args, **kwargs)
         # assigning permissions AFTER the user is added in the database
         self.user_permissions.add(
             *[Permission.objects.get(codename=i) for i in self._predefined_permissions]
@@ -45,30 +26,33 @@ class User(AbstractUser, Approvable):
         return saved_user
 
 
-class Admin(User):
-    _base_role = Role.ADMIN
+# ALL USER MODELS
+class AdminUserManager(UserManager):
+    def get_queryset(self) -> models.QuerySet[AbstractUser]:
+        return super().get_queryset().filter(is_superuser=True)
 
-    institute = models.CharField(max_length=256)
+
+class Admin(User):
+    objects = AdminUserManager()
 
     class Meta:
+        proxy = True
         verbose_name = "Admin"
         verbose_name_plural = "Admins"
 
-    def save(
-        self,
-        force_insert: bool = False,
-        force_update: bool = False,
-        using: Optional[str] = None,
-        update_fields: Optional[Iterable[str]] = None,
-    ) -> None:
-        self.is_staff = True
-        self.is_superuser = True
-        self.is_approved = True
-        return super().save(force_insert, force_update, using, update_fields)
+    def save(self, *args, **kwargs) -> None:
+        """
+        `self.pk` // primary key doesnt exist when model
+        is not yet saved in database
+        """
+        if not self.pk:
+            self.is_staff = True
+            self.is_superuser = True
+            self.is_approved = True
+        return super().save(*args, **kwargs)
 
 
 class Student(User):
-    _base_role = Role.STUDENT
     marks = models.IntegerField()
     institute = models.CharField(max_length=256)
     department = models.CharField(max_length=256)
@@ -83,12 +67,10 @@ class Student(User):
 
 
 class Volunteer(User):
-    _base_role = Role.VOLUNTEER
     job_numbers = models.PositiveSmallIntegerField()
     department = models.CharField(max_length=256)
     semester = models.CharField(max_length=1, blank=False)
-    volunteer_type = models.PositiveSmallIntegerField(
-        choices=VolunteerType.choices)
+    volunteer_type = models.PositiveSmallIntegerField(choices=VolunteerType.choices)
     reference = models.TextField(max_length=2000, null=True)
 
     _predefined_permissions = ["view_volunteer"]
@@ -99,7 +81,6 @@ class Volunteer(User):
 
 
 class DeptOfficer(User):
-    _base_role = Role.DEPT_OFFICER
     department = models.CharField(max_length=256)
     address = models.TextField(max_length=2000)
 
