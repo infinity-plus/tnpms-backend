cda1031a7810df0bffe76af2c95298ce502ae04f
jeelpatel231 <jeelpatel231@gmail.com>
2023-03-06 12:57:03 +0530

    remove redundant role class

--- backend/user/roles.py
+++ backend/user/roles.py
@@ -1,14 +1,6 @@
 from django.db.models import IntegerChoices
 
 
-class Role(IntegerChoices):
-    ADMIN = 1, "Admin"
-    STUDENT = 2, "Student"
-    VOLUNTEER = 3, "Volunteer"
-    DEPT_OFFICER = 4, "Department Officer"
-    UNDEFINED = 5, "Undefined"
-
-
 class VolunteerType(IntegerChoices):
     LEADER = 1, "Leader"
     WORKER = 2, "Worker"
