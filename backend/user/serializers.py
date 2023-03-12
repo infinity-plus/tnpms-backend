49cc96604e85e89972bb583e7e1ec164168f84e8
jeelpatel231 <jeelpatel231@gmail.com>
2023-03-13 00:06:30 +0530

    set fields readonly for Student serializer

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -3,9 +3,14 @@ from user import models as m
 from tnpapp.serializers import BaseUserModelSerializer
 
 
+# https://github.com/encode/django-rest-framework/issues/1926
 class StudentSerializer(BaseUserModelSerializer):
     class Meta(BaseUserModelSerializer.Meta):
         model = m.Student
+        extra_kwargs = {
+            "is_selected": {"read_only": True},
+            "is_profile_complete": {"read_only": True},
+        }
 
 
 class DeptOfficerSerializer(BaseUserModelSerializer):
