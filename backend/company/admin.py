384f8f01f5b99fe8c8d9cc701b8259abf70a743e
jeelpatel231 <jeelpatel231@gmail.com>
2023-03-15 23:58:43 +0530

    PROTOTYPE : make mass_email admin action for notifying users

--- backend/company/admin.py
+++ backend/company/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from company import models as m
 from django.contrib.admin import ModelAdmin
+from tnpapp import settings
+from user.models import Student
+from django.core.mail import send_mass_mail
 
 # Register your models here.
 
@@ -18,8 +21,24 @@ class CompanyAdmin(ModelAdmin):
 
 @admin.register(m.CurrentOpening)
 class CurrentOpeningsAdmin(ModelAdmin):
+    # TODO : generate message body and send emails to filtered out students
+    @admin.action(description="Notify students")
+    def notify_students_opening(self, request, queryset):
+        interested_students = Student.objects.filter(is_blocked=False)
+        student_emails = [s.email for s in interested_students]
+        # make multiple message bodies from queryset and use send_mass_mail
+        all_emails = []
+        for i in queryset:
+            print(i)
+            # construct message body using title and description of the opening
+            # all_emails.append(message_body)
+        # send_mass_mail(all_emails, fail_silently=False)
+
+    actions = ("notify_students_opening",)
+
     list_display = (
         "job_title",
+        "company",
         "opening_year",
         "nature_of_job",
         "vacancy_count",
